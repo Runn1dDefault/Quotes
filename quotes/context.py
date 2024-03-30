75c92759120e2eaa399ac6ee7842775e54d18850
dinnurnarmatov <dinnur.narmatov@data-ox.com>
2024-03-31 03:18:50 +0600

    added ws connection on index page

--- quotes/context.py
+++ quotes/context.py
@@ -1,4 +1,10 @@
+from urllib.parse import urlparse
+
+
 def base_context_processor(request):
+    base_url = request.build_absolute_uri("/").rstrip("/")
+    url = urlparse(base_url)
     return {
-        'BASE_URL': request.build_absolute_uri("/").rstrip("/")
+        'BASE_URL': base_url,
+        "HOST": base_url.replace(url.scheme + "://", "")
     }
