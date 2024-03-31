5d109f7bb522bb5927e432651477393da4c559cf
dinnurnarmatov <dinnur.narmatov@data-ox.com>
2024-03-31 17:10:28 +0600

    added author fullname to notification of new quote

--- notifications/signals.py
+++ notifications/signals.py
@@ -8,4 +8,7 @@ from quotes.models import Quote
 @receiver(post_save, sender=Quote)
 def notification_on_new_quote(sender, instance, created, **kwargs):
     if created:
-        send_notification("Created new quote", quote_id=str(instance.id))
+        send_notification(
+            f"Created a new quote from {instance.author.full_name}",
+            quote_id=str(instance.id)
+        )
